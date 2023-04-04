e39b9b69f8aab4d7d6ec855bee398ddb37491d69
Alejandro Osornio <50227494+AOx0@users.noreply.github.com>
2023-04-03 22:41:40 -0600

    Rewrite to leptos

--- src/main.rs
+++ src/main.rs
@@ -1,205 +1,30 @@
 #![allow(non_snake_case)]
-
-use dioxus::prelude::*;
-use graph::*;
+use leptos::*;
 
 fn main() {
     wasm_logger::init(wasm_logger::Config::new(log::Level::Info));
-    dioxus_web::launch(App);
+    mount_to_body(|cx| view! { cx,  <SimpleCounter initial_value=3 /> })
 }
 
-#[inline_props]
-fn MoonIcon(cx: Scope) -> Element {
-    cx.render(rsx! {
-        svg {
-            height: "1em",
-            view_box: "0 0 50 50",
-            width: "1em",
-            xmlns: "http://www.w3.org/2000/svg",
-            path {
-                d: "M 43.81 29.354 C 43.688 28.958 43.413 28.626 43.046 28.432 C 42.679 28.238 42.251 28.198 41.854 28.321 C 36.161 29.886 30.067 28.272 25.894 24.096 C 21.722 19.92 20.113 13.824 21.683 8.133 C 21.848 7.582 21.697 6.985 21.29 6.578 C 20.884 6.172 20.287 6.022 19.736 6.187 C 10.659 8.728 4.691 17.389 5.55 26.776 C 6.408 36.163 13.847 43.598 23.235 44.451 C 32.622 45.304 41.28 39.332 43.816 30.253 C 43.902 29.96 43.9 29.647 43.81 29.354 Z",
-                fill: "currentColor",
-            }
-        }
-    })
-}
+#[component]
+pub fn SimpleCounter(cx: Scope, initial_value: i32) -> impl IntoView {
+    // create a reactive signal with the initial value
+    let (value, set_value) = create_signal(cx, initial_value);
 
-#[inline_props]
-fn SunIcon(cx: Scope) -> Element {
-    cx.render(rsx! {
-        svg {
-            width: "1em", view_box: "0 0 24 24",
-            height: "1em", fill: "none",
-            xmlns: "http://www.w3.org/2000/svg",
-            circle { r: "5.75375", fill: "currentColor", cx: "11.9998", cy: "11.9998" }
-            g {
-                circle { transform: "rotate(-60 3.08982 6.85502)", fill: "currentColor", cx: "3.08982", cy: "6.85502", r: "1.71143" }
-                circle { r: "1.71143", cx: "3.0903", cy: "17.1436", transform: "rotate(-120 3.0903 17.1436)", fill: "currentColor" }
-                circle { r: "1.71143", cx: "12", cy: "22.2881", fill: "currentColor" }
-                circle { transform: "rotate(-60 20.9101 17.1436)", cy: "17.1436", cx: "20.9101", r: "1.71143", fill: "currentColor" }
-                circle { cy: "6.8555", r: "1.71143", fill: "currentColor", cx: "20.9101", transform: "rotate(-120 20.9101 6.8555)" }
-                circle { fill: "currentColor", cy: "1.71143", r: "1.71143", cx: "12" }
-            }
-        }
-    })
-}
+    // create event handlers for our buttons
+    // note that `value` and `set_value` are `Copy`, so it's super easy to move them into closures
+    let clear = move |_| set_value.set(0);
+    let decrement = move |_| set_value.update(|value| *value -= 1);
+    let increment = move |_| set_value.update(|value| *value += 1);
 
-#[inline_props]
-fn Input<'a>(cx: Scope, placeholder: &'a str, value: UseState<String>) -> Element {
-    cx.render(rsx!{
-        div {
-            class: "w-full flex",
-            input { class: "flex-1 grow mx-4 !outline-none focus:outline-0 rounded-md p-2 bg-[#f6f8fa] dark:bg-[#010409] border dark:border-[#282d34]",
-                spellcheck:"false",
-                placeholder: "{placeholder}",
-                value: "{value}",
-                oninput: move |evt| {
-                    value.set(evt.value.clone());
-                }
-            }
-        }
-    })
-}
-
-#[inline_props]
-fn TextArea<'a>(cx: Scope, placeholder: &'a str, value: UseState<String>) -> Element {
-    cx.render(rsx!{
-        div {
-            class: "w-full h-full flex",
-            textarea { 
-                style: "font-size: 12px;",
-                spellcheck:"false",
-                class: "flex-1 grow !outline-none font-mono resize-none focus:outline-0 ml-4 md:ml-0 mb-4 mr-4 rounded-md p-2 bg-[#f6f8fa] dark:bg-[#010409] border dark:border-[#282d34]",
-                placeholder: "{placeholder}",
-                value: "{value}",
-                oninput: move |evt| {
-                    value.set(evt.value.clone());
-                }
-            }
-        }
-    })
-}
-
-fn App(cx: Scope) -> Element {
-    let SCRIPT = 
-r#"const html = document.getElementsByTagName('html')[0];
-if(html.classList.contains('dark')) {
-    document.getElementById('tcolor').content = 'black'
-    html.classList.remove('dark');
-    localStorage.theme = 'light'
-} else {
-    document.getElementById('tcolor').content = 'rgb(31 41 55 / var(--tw-bg-opacity))'
-    html.classList.add('dark');
-    localStorage.theme = 'dark'
-}"#;
-
-    let graph: Option<Graph<&str, (f64, f64), u64>> = None;
-    let graph_description = use_state(cx, || {
-        r##"{ "nodes": [
-    { "name":"Arad",
-      "goestTo": [
-          { "name": "Sibiu", "weight": 5 },
-          { "name": "Sibiu", "weight": 5 },
-          { "name": "Sibiu", "weight": 5 },
-          { "name": "Sibiu", "weight": 5 }
-      ]
-    },
-    { "name":"Arad",
-      "goestTo": [
-          { "name": "Sibiu", "weight": 5 },
-          { "name": "Sibiu", "weight": 5 },
-          { "name": "Sibiu", "weight": 5 },
-          { "name": "Sibiu", "weight": 5 }
-      ]
+    // create user interfaces with the declarative `view!` macro
+    view! {
+        cx,
+        <div class="flex flex-col space-y-5 items-center">
+            <button on:click=clear>"Clear"</button>
+            <button on:click=decrement>"-1"</button>
+            <span>"Value: " {value} "!"</span>
+            <button on:click=increment>"+1"</button>
+        </div>
     }
-]}"##
-            .to_owned()
-    });
-    let output = use_state(cx, || String::new());
-    
-    let entrada = use_state(cx, || String::new());
-    let salida = use_state(cx, || String::new());
-    let metodo = use_state(cx, || String::new());
-
-    let active =  use_state(cx, || 1);
-    let activ2 = use_state(cx, || "bg-[#f6f8fa] dark:bg-gray-900");
-    let activ = use_state(cx, || "bg-[#c1c2c4] dark:bg-gray-700");
-    let textarea_description = use_state(cx, || "Graph description...".to_owned());
-    let textarea_text = use_state(cx, || graph_description.get().clone());
-    
-    cx.render(rsx! {
-        div {
-            class: "py-5 flex justify-between mx-5 items-center",
-            h1 {
-                class: "text-lg md:text-2xl font-bold",
-                "aigraph1/"
-            }
-            button {
-                class: "bg-[#f6f8fa] dark:bg-gray-900 rounded py-2 px-4",
-                "onclick": "{SCRIPT}",
-                div {
-                    class: "flex items-center space-x-3 text-black dark:text-white",
-                    p { "Theme" }
-                    div {
-                        div { class: "dark:hidden block",
-                            SunIcon {}
-                        }
-                        div { class: "hidden dark:block",
-                            MoonIcon {}
-                        }
-                    }
-                }
-            }
-        }
-        div {
-            class: "pb-2 flex justify-between mx-5 items-center",
-            div {}
-            div {
-                class: "bg-[#f6f8fa] dark:bg-gray-900 rounded py",
-                button {
-                    class: "{activ} rounded py px-4",
-                    onclick: move |_| {
-                        if *active.get() != 1 {
-                            activ.with_mut(|v| activ2.with_mut(|v2| std::mem::swap(v, v2) ));
-                            textarea_text.set(graph_description.get().clone());
-                            textarea_description.set("Graph description...".to_owned());
-                            active.set(1);
-                        }
-                    },
-                    div {
-                        class: "flex items-center space-x-3 text-black dark:text-white",
-                        p { "Graph" }
-                    }
-                }
-                button {
-                    class: "{activ2} rounded py px-4",
-                    onclick: move |_| {
-                        if *active.get() != 2 {
-                            activ.with_mut(|v| activ2.with_mut(|v2| std::mem::swap(v, v2) ));
-                            textarea_text.set(output.get().clone());
-                            textarea_description.set("Console output...".to_owned());
-                            active.set(2);
-                        }
-                    },
-                    div {
-                        class: "flex items-center space-x-3 text-black dark:text-white",
-                        p { "Console" }
-                    }
-                }
-            }
-        }
-        div {
-            class: "flex flex-col md:flex-row sm:flex-col w-full h-full space-y-5 sm:space-y-5 md:space-y-0 placeholder:text-[#c9d1d9] text-[#24292f] dark:text-[#c9d1d9]",
-            div {
-                class: "flex flex-col text-sm items-center justify-start w-full md:w-1/3 sm:w-full space-y-5",
-                Input { placeholder: "Starting edge...", value: entrada.clone() }
-                Input { placeholder: "Ending edge...", value: salida.clone() }
-                Input { placeholder: "Search method...", value: metodo.clone() }
-            }
-            div {
-                class: "w-full h-full md:w-2/3 sm:w-full",
-                TextArea { placeholder: "{textarea_description}", value: textarea_text.clone() }
-            }
-        }
-    })
 }
