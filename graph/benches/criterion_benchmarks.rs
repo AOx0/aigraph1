82af0d878417c0473f06eac3c8cfceef36e13406
Alejandro Osornio <50227494+AOx0@users.noreply.github.com>
2023-04-22 02:30:43 -0600

    Fix criterion benchmarks

--- graph/benches/criterion_benchmarks.rs
+++ graph/benches/criterion_benchmarks.rs
@@ -1,133 +1,40 @@
 #![allow(dead_code)]
 
 use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion};
-use graph::*;
-
-fn test_graph() -> Graph<&'static str, (), u16> {
-    let graph: Graph<&'static str, (), u16> = graph! {
-        with_node: (),
-        with_edges: next,
-        nodes: [
-            "Acapulco",              "Villa Hermosa",       "Guanajuato",                 "Cancun",               "Chilpancingo",
-            "Aguaprieta",            "Alvarado",            "Valladolid",                 "Acayucan",             "Santa Ana",
-            "Oaxaca",                "Chetumal",            "Tehuantepec",                "Aguascalientes",       "Atlacomulco",
-            "Campeche",              "Tuxtla",              "Guadalajara",                "Queretaro",            "Felipe Carrillo Puerto",
-            "Merida",                "Chihuahua",           "Janos",                      "Juarez",               "Ojinaga",
-            "Iguala",                "Ciudad Altamirano",   "Cuernavaca",                 "Toluca de Lerdo",      "Zihuatanejo",
-            "Ciudad del Carmen",     "Ciudad Obregon",      "Guaymas",                    "Ciudad Victoria",      "Matamoros",
-            "Soto la Marina",        "Tampico",             "Colima",                     "Morelia",              "Playa Azul",
-            "Cordoba",               "Veracruz",            "Culiacan",                   "Hidalgo del Parral",   "Topolobampo",
-            "Durango",               "Mazatlan",            "Torreon",                    "Ensenada",             "San Quintin",
-            "Francisco Escarcega",   "Manzanillo",          "Salamanca",                  "Hermosillo",           "San Luis Potosi",
-            "Izucar de Matamoros",   "La Paz",              "Cabo San Lucas",             "Reynosa",              "Mexicalli",
-            "San Felipe",            "Tijuana",             "Ciudad de Mexico",           "Pachuca de Soto",      "Puebla",
-            "Tlaxcala",              "Monclova",            "Piedras Negras",             "Monterrey",            "Nuevo Laredo",
-            "Puerto Angel",          "Tehuacan",            "Tuxpan de Rodriguez Cano",   "Pinotepa Nacional",    "Zacatecas",
-            "Santa Rosalia",         "Santo Domingo",       "Tepic",                      "Ciudad Juarez"
-        ],
-        connections: [
-            "Cancun" => {(90) "Valladolid", (100) "Felipe Carrillo Puerto"},
-            "Valladolid" => {(90) "Felipe Carrillo Puerto"},
-            "Felipe Carrillo Puerto" => {(60) "Campeche"},
-            "Campeche" => {(90) "Merida", (100) "Chetumal", (90) "Ciudad del Carmen"},
-            "Chetumal" => {(111) "Francisco Escarcega"},
-            "Ciudad del Carmen" => {(90) "Villa Hermosa", (90) "Tuxtla"},
-            "Villa Hermosa" => {(90) "Acayucan"},
-            "Tuxtla" => {(90) "Acayucan"},
-            "Acayucan" => {(80) "Tehuantepec", (110) "Alvarado"},
-            "Alvarado" => {(100) "Oaxaca"},
-            "Oaxaca" => {(80) "Tehuacan", (90) "Puerto Angel", (90) "Izucar de Matamoros"},
-            "Puerto Angel" => {(100) "Pinotepa Nacional" },
-            "Izucar de Matamoros" => {(90) "Puebla", (100) "Cuernavaca"},
-            "Pinotepa Nacional" => {(100) "Acapulco"},
-            "Cuernavaca" => {(100) "Ciudad de Mexico", (100) "Ciudad Altamirano"},
-            "Puebla" => {(90) "Ciudad de Mexico", (80) "Cordoba"},
-            "Acapulco" => {(140) "Chilpancingo"},
-            "Ciudad de Mexico" => {(100) "Tlaxcala", (110) "Toluca de Lerdo", (90) "Queretaro", (100) "Pachuca de Soto"},
-            "Ciudad Altamirano" => {(90) "Zihuatanejo"},
-            "Cordoba" => {(90) "Veracruz"},
-            "Chilpancingo" => {(90) "Iguala"},
-            "Toluca de Lerdo" => {(100) "Ciudad Altamirano"},
-            "Queretaro" => {(90) "Atlacomulco", (90) "Salamanca", (90) "San Luis Potosi"},
-            "Pachuca de Soto" => {(110) "Tuxpan de Rodriguez Cano"},
-            "Zihuatanejo" => {(90) "Playa Azul"},
-            "Iguala" => {(100) "Cuernavaca", (110) "Ciudad Altamirano"},
-            "Salamanca" => {(90) "Guanajuato", (90) "Guadalajara"},
-            "San Luis Potosi" => {(90) "Zacatecas", (70) "Durango", (100) "Aguascalientes" },
-            "Tuxpan de Rodriguez Cano" => {(100) "Tampico"},
-            "Playa Azul" => {(100) "Morelia", (100) "Colima", (100) "Manzanillo"},
-            "Guanajuato" => {(80) "Aguascalientes"},
-            "Guadalajara" => {(110) "Tepic"},
-            "Aguascalientes" =>{(70) "Guadalajara"},
-            "Durango" => {(90) "Hidalgo del Parral", (90) "Mazatlan"},
-            "Tampico" => {(80) "Ciudad Victoria"},
-            "Morelia" => {(90) "Salamanca"},
-            "Manzanillo" => {(50) "Colima", (80) "Guadalajara"},
-            "Colima" => {(90) "Morelia", (50) "Guadalajara"},
-            "Tepic" =>{(50) "Mazatlan"},
-            "Hidalgo del Parral" => {(130) "Chihuahua", (110) "Topolobampo", (80) "Culiacan"},
-            "Mazatlan" => {(90) "Culiacan"},
-            "Ciudad Victoria" => {(80) "Soto la Marina", (80) "Matamoros", (80) "Monterrey", (80) "Durango"},
-            "Chihuahua" => {(90) "Ciudad Juarez", (90) "Janos"},
-            "Topolobampo" => {(90) "Ciudad Obregon"},
-            "Culiacan" => {(110) "Topolobampo"},
-            "Matamoros" => {(90) "Reynosa"},
-            "Monterrey" => {(110) "Nuevo Laredo",(70) "Monclova"},
-            "Janos" => {(110) "Aguaprieta"},
-            "Ciudad Obregon" => {(80) "Guaymas"},
-            "Reynosa" => {(100) "Nuevo Laredo"},
-            "Nuevo Laredo" => {(100) "Piedras Negras"},
-            "Monclova" => {(100) "Torreon", (90) "Ojinaga"},
-            "Aguaprieta" => {(90) "Santa Ana"},
-            "Guaymas" => {(90) "Hermosillo"},
-            "Piedras Negras" => {(90) "Monclova"},
-            "Torreon" => {(90) "Durango"},
-            "Ojinaga" => {(90) "Chihuahua"},
-            "Santa Ana" => {(159) "Mexicalli"},
-            "Hermosillo" => {(100) "Santa Ana"},
-            "Mexicalli" => {(50) "Tijuana", (70) "San Felipe"},
-            "Tijuana" => {(30) "Ensenada"},
-            "San Felipe" => {(50) "Ensenada"},
-            "Ensenada" => {(90) "San Quintin"},
-            "San Quintin" => {(140) "Santa Rosalia"},
-            "Santa Rosalia" => {(100) "Santo Domingo"},
-            "Santo Domingo" => {(100) "La Paz"},
-            "La Paz" => {(40) "Cabo San Lucas"}
-        ]
-    };
-    graph
-}
+use graph::walkers::*;
 
 fn breadth_test1(c: &mut Criterion) {
-    let graph = black_box(test_graph());
+    let graph = black_box(test_graph2());
     c.bench_function("breadth_full", |a| {
         a.iter(|| {
             graph
-                .breadth_first(
-                    black_box(graph.name_index("Cancun").unwrap()),
-                    Some(10000.into()),
-                )
+                .perform_search(BreadthFirst::new(
+                    &graph,
+                    graph.journey("Cancun", None).unwrap(),
+                    Direction::Outgoing,
+                ))
                 .ok()
         })
     });
 }
 
 fn breadth_test2(c: &mut Criterion) {
-    let graph = black_box(test_graph());
+    let graph = black_box(test_graph2());
     c.bench_function("breadth_arad_neamt", |a| {
         a.iter(|| {
             graph
-                .breadth_first(
-                    black_box(graph.name_index("Cancun").unwrap()),
-                    black_box(Some(graph.name_index("Cabo San Lucas").unwrap())),
-                )
+                .perform_search(BreadthFirst::new(
+                    &graph,
+                    graph.journey("Cancun", Some("Cabo San Lucas")).unwrap(),
+                    Direction::Outgoing,
+                ))
                 .ok()
         })
     });
 }
 
 fn breadth_test3(c: &mut Criterion) {
-    let graph = black_box(test_graph());
+    let graph = black_box(test_graph2());
     let mut group = c.benchmark_group("Evolution breadth_first");
     for (depth, city) in [
         (26, "Cabo San Lucas"),
@@ -139,7 +46,7 @@ fn breadth_test3(c: &mut Criterion) {
         (20, "San Felipe"),
         (19, "Mexicalli"),
         (18, "Santa Ana"),
-        (17, "Aguaprieta"),
+        (17, "Agua Prieta"),
         (16, "Janos"),
         (15, "Chihuahua"),
         (14, "Hidalgo del Parral"),
@@ -162,17 +69,18 @@ fn breadth_test3(c: &mut Criterion) {
         group.bench_with_input(BenchmarkId::from_parameter(depth), city, |b, name| {
             b.iter(|| {
                 graph
-                    .breadth_first(
-                        graph.name_index("Cancun").unwrap(),
-                        Some(graph.name_index(name).unwrap()),
-                    )
+                    .perform_search(BreadthFirst::new(
+                        &graph,
+                        graph.journey("Cancun", Some(name)).unwrap(),
+                        Direction::Outgoing,
+                    ))
                     .unwrap()
             })
         });
     }
 }
 fn depht_test3(c: &mut Criterion) {
-    let graph = black_box(test_graph());
+    let graph = black_box(test_graph2());
     let mut group = c.benchmark_group("Evolution depth_first");
     for (depth, city) in [
         (26, "Cabo San Lucas"),
@@ -184,7 +92,7 @@ fn depht_test3(c: &mut Criterion) {
         (20, "San Felipe"),
         (19, "Mexicalli"),
         (18, "Santa Ana"),
-        (17, "Aguaprieta"),
+        (17, "Agua Prieta"),
         (16, "Janos"),
         (15, "Chihuahua"),
         (14, "Hidalgo del Parral"),
@@ -207,18 +115,19 @@ fn depht_test3(c: &mut Criterion) {
         group.bench_with_input(BenchmarkId::from_parameter(depth), city, |b, name| {
             b.iter(|| {
                 graph
-                    .depth_first(
-                        graph.name_index("Cancun").unwrap(),
-                        Some(graph.name_index(name).unwrap()),
-                        None::<usize>,
-                    )
+                    .perform_search(DepthFirst::new(
+                        &graph,
+                        graph.journey("Cancun", Some(name)).unwrap(),
+                        None::<u8>,
+                        Direction::Outgoing,
+                    ))
                     .unwrap()
             })
         });
     }
 }
 fn dijkstra_test3(c: &mut Criterion) {
-    let graph = black_box(test_graph());
+    let graph = black_box(test_graph2());
     let mut group = c.benchmark_group("Evolution dijkstra");
     for (depth, city) in [
         (27, "Cabo San Lucas"),
@@ -230,7 +139,7 @@ fn dijkstra_test3(c: &mut Criterion) {
         (21, "Tijuana"),
         (20, "Mexicalli"),
         (19, "Santa Ana"),
-        (18, "Aguaprieta"),
+        (18, "Agua Prieta"),
         (17, "Janos"),
         (16, "Chihuahua"),
         (15, "Hidalgo del Parral"),
@@ -253,11 +162,12 @@ fn dijkstra_test3(c: &mut Criterion) {
         group.bench_with_input(BenchmarkId::from_parameter(depth), city, |b, name| {
             b.iter(|| {
                 graph
-                    .dijkstra_impl(
-                        graph.name_index("Cancun").unwrap(),
-                        Some(graph.name_index(name).unwrap()),
-                        |s| *s,
-                    )
+                    .perform_search(dijkstra::new(
+                        &graph,
+                        black_box(graph.journey("Cancun", Some(name)).unwrap()),
+                        |e| *e,
+                        Direction::Outgoing,
+                    ))
                     .unwrap()
             })
         });
@@ -265,7 +175,7 @@ fn dijkstra_test3(c: &mut Criterion) {
 }
 
 fn bidirectional_test3(c: &mut Criterion) {
-    let graph = black_box(test_graph());
+    let graph = black_box(test_graph2());
     let mut group = c.benchmark_group("Evolution bidirectional");
     for (depth, city) in [
         // expect nodes
@@ -278,7 +188,7 @@ fn bidirectional_test3(c: &mut Criterion) {
         (21, "Tijuana"),
         (20, "Mexicalli"),
         (19, "Santa Ana"),
-        (18, "Aguaprieta"),
+        (18, "Agua Prieta"),
         (17, "Janos"),
         (16, "Chihuahua"),
         (15, "Hidalgo del Parral"),
@@ -300,19 +210,17 @@ fn bidirectional_test3(c: &mut Criterion) {
         group.throughput(criterion::Throughput::Elements(depth));
         group.bench_with_input(BenchmarkId::from_parameter(depth), city, |b, name| {
             b.iter(|| {
-                let a = Dijkstra::new(
+                let a = dijkstra::new(
                     &graph,
-                    graph.name_index("Cancun").unwrap(),
-                    Some(graph.name_index(name).unwrap()),
+                    graph.journey("Cancun", Some(name)).unwrap(),
+                    |e| *e,
                     Direction::Outgoing,
-                    |e| *e,
                 );
-                let b = Dijkstra::new(
+                let b = dijkstra::new(
                     &graph,
-                    graph.name_index(name).unwrap(),
-                    Some(graph.name_index("Cancun").unwrap()),
-                    Direction::Incoming,
+                    graph.journey("Cancun", Some(name)).unwrap(),
                     |e| *e,
+                    Direction::Incoming,
                 );
                 graph.bidirectional(a, b).unwrap();
             })
@@ -321,37 +229,39 @@ fn bidirectional_test3(c: &mut Criterion) {
 }
 
 fn depth_test1(c: &mut Criterion) {
-    let graph = black_box(test_graph());
+    let graph = black_box(test_graph2());
     c.bench_function("depth_full", |a| {
         a.iter(|| {
             graph
-                .depth_first::<u32>(
-                    black_box(graph.name_index("Cancun").unwrap()),
-                    Some(10000.into()),
-                    None,
-                )
+                .perform_search(DepthFirst::new(
+                    &graph,
+                    graph.journey("Cancun", None).unwrap(),
+                    None::<u8>,
+                    Direction::Outgoing,
+                ))
                 .ok()
         })
     });
 }
 
 fn depth_test2(c: &mut Criterion) {
-    let graph = black_box(test_graph());
+    let graph = black_box(test_graph2());
     c.bench_function("depth_arad_neamt", |a| {
         a.iter(|| {
             graph
-                .depth_first::<u32>(
-                    black_box(graph.name_index("Cancun").unwrap()),
-                    black_box(Some(graph.name_index("Cabo San Lucas").unwrap())),
-                    None,
-                )
+                .perform_search(DepthFirst::new(
+                    &graph,
+                    graph.journey("Cancun", Some("Cabo San Lucas")).unwrap(),
+                    None::<u8>,
+                    Direction::Outgoing,
+                ))
                 .ok()
         })
     });
 }
 
 fn depth_test3(c: &mut Criterion) {
-    let graph = black_box(test_graph());
+    let graph = black_box(test_graph2());
     let mut group = c.benchmark_group("Evolution breadth_first");
     for (depth, city) in [
         (40, "Cabo San Lucas"),
@@ -399,11 +309,12 @@ fn depth_test3(c: &mut Criterion) {
         group.bench_with_input(BenchmarkId::from_parameter(depth), city, |b, name| {
             b.iter(|| {
                 graph
-                    .depth_first(
-                        graph.name_index("Cancun").unwrap(),
-                        Some(graph.name_index(name).unwrap()),
+                    .perform_search(DepthFirst::new(
+                        &graph,
+                        graph.journey("Cancun", Some(name)).unwrap(),
                         None::<u8>,
-                    )
+                        Direction::Outgoing,
+                    ))
                     .unwrap()
             })
         });
@@ -411,30 +322,32 @@ fn depth_test3(c: &mut Criterion) {
 }
 
 fn dijkstra_test1(c: &mut Criterion) {
-    let graph = black_box(test_graph());
+    let graph = black_box(test_graph2());
     c.bench_function("dijkstra_full", |a| {
         a.iter(|| {
             graph
-                .dijkstra_impl(
-                    black_box(graph.name_index("Cancun").unwrap()),
-                    Some(10000.into()),
+                .perform_search(dijkstra::new(
+                    &graph,
+                    black_box(graph.journey("Cancun", None).unwrap()),
                     |e| *e,
-                )
+                    Direction::Outgoing,
+                ))
                 .ok()
         })
     });
 }
 
 fn dijkstra_test2(c: &mut Criterion) {
-    let graph = black_box(test_graph());
+    let graph = black_box(test_graph2());
     c.bench_function("dijkstra_arad_neamt", |a| {
         a.iter(|| {
             graph
-                .dijkstra_impl(
-                    black_box(graph.name_index("Cancun").unwrap()),
-                    black_box(Some(graph.name_index("Cabo San Lucas").unwrap())),
+                .perform_search(dijkstra::new(
+                    &graph,
+                    black_box(graph.journey("Cancun", Some("Cabo San Lucas")).unwrap()),
                     |e| *e,
-                )
+                    Direction::Outgoing,
+                ))
                 .ok()
         })
     });
@@ -443,24 +356,24 @@ fn dijkstra_test2(c: &mut Criterion) {
 fn construct_graph(c: &mut Criterion) {
     c.bench_function("graph_construct", |a| {
         a.iter(|| {
-            test_graph();
+            test_graph2();
         })
     });
 }
 
 criterion_group!(
     benches,
-    // breadth_test1,
-    // breadth_test2,
-    // breadth_test3,
-    // depth_test1,
-    // depth_test2,
-    // depth_test3,
-    // dijkstra_test1,
-    // dijkstra_test2,
-    // dijkstra_test3,
-    // bidirectional_test3,
-    // construct_graph
+    breadth_test1,
+    breadth_test2,
+    breadth_test3,
+    depth_test1,
+    depth_test2,
+    depth_test3,
+    dijkstra_test1,
+    dijkstra_test2,
+    dijkstra_test3,
+    bidirectional_test3,
+    construct_graph
 );
 
 criterion_main!(benches);
